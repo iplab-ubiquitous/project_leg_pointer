b4ba30fe8d97d413c43951d31f8d15d68c1d2f40
Yuu ichikawa <amylasef85@gmail.com>
2018-09-15 18:56:56 +0900

    2次元で動くようにした

--- IR/IR_main.py
+++ IR/IR_main.py
@@ -7,8 +7,10 @@ from PyQt5.QtGui import QPainter, QFont
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal
 #PySerial
 import serial
+import pyautogui
 
-window_size = 1280
+window_size_x = 1280
+window_size_y = 720
 #window_size = QtGui.qApp.desktop().width()
 
 
@@ -18,10 +20,11 @@ class sensor_read:
         for i in range(10):
             self.ser.readline()  # 読み飛ばし(欠けたデータが読み込まれるのを避ける)
 
-    def read_test_ser(self):
+    def read_test_ser(self, dir):
         line = float(0)
-        while self.ser.in_waiting > 0:
-            line = self.ser.readline().strip().decode('utf-8')  # .split(",")
+        while self.ser.in_waiting > 1 or line == float(0):
+            lst = self.ser.readline().strip().decode('utf-8').split(",")
+            line = lst[dir]
         #line_f = [float(s) for s in self.line]
         #return line_f
 
@@ -32,16 +35,25 @@ class draw_gui(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.old_x = window_size/2
+        self.pos_x0 = 0
+        self.pos_x1 = 0
+        self.pos_y0 = 0
+        self.pos_y1 = 0
+        self.old_x = window_size_x/2
         self.new_x = 0
-        self.resize(window_size, int(window_size*16/10))
+        self.old_y = window_size_y/2
+        self.new_y = 0
+        self.resize(window_size_x, window_size_y)
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.value_upd)
+        #self.timer.timeout.connect(self.value_upd_y)
         self.timer.start(50)  # 20Hz
 
         self.button_left_calb = QPushButton(self)
         self.button_right_calb = QPushButton(self)
+        self.button_up_calb = QPushButton(self)
+        self.button_down_calb = QPushButton(self)
         self.button_reset = QPushButton(self)
 
         self.button_left_calb.move(0, 50)
@@ -54,6 +66,16 @@ class draw_gui(QWidget):
         self.button_right_calb.clicked.connect(self.calibration_right)
         self.right_calb_flag = False
 
+        self.button_up_calb.move(0, 100)
+        self.button_up_calb.setText("キャリブレーション:上")
+        self.button_up_calb.clicked.connect(self.calibration_up)
+        self.up_calb_flag = False
+
+        self.button_down_calb.move(200, 100)
+        self.button_down_calb.setText("キャリブレーション:下")
+        self.button_down_calb.clicked.connect(self.calibration_down)
+        self.down_calb_flag = False
+
         self.button_reset.move(400, 50)
         self.button_reset.setText("リセット")
         self.button_reset.clicked.connect(self.calibration_reset)
@@ -61,13 +83,17 @@ class draw_gui(QWidget):
         self.textbox = QLineEdit(self)
         self.textbox.move(10, 10)
 
+        #pyautogui.FAILSAFE = False
+
     #描画と数値計算を直列でやっている
     def paintEvent(self, QPaintEvent):
         painter = QPainter(self)
-        self.textbox.setText(str(self.new_x))  # 座標確認用
+        self.textbox.setText(str(self.new_x-640))  # 座標確認用
         painter.setPen(Qt.black)
         painter.setBrush(Qt.red)
-        painter.drawRect(self.new_x, 400, 20, 20)
+        painter.drawRect(self.new_x, self.new_y, 20, 20)
+        pyautogui.moveTo(self.new_x, self.new_y)
+        
         '''
         painter.drawRect(100, 400, 100, 100)
         painter.drawRect(250, 400, 100, 100)
@@ -89,35 +115,81 @@ class draw_gui(QWidget):
     def calibration_left(self):
         if not self.left_calb_flag:
             lst = []
-            for i in range(200):
-                val = rd.read_test_ser()
+            for i in range(100):
+                val = rd.read_test_ser(0)
                 lst.append(val)
-            pos_x0 = min(lst)
+            self.pos_x0 = max(lst)
             self.left_calb_flag = True
+            print(lst)
 
     def calibration_right(self):
         if not self.right_calb_flag:
             lst = []
-            for i in range(200):
-                val = rd.read_test_ser()
+            for i in range(100):
+                val = rd.read_test_ser(0)
                 lst.append(val)
-            pos_x1 = max(lst)
+            self.pos_x1 = min(lst)
             self.right_calb_flag = True
+            print(lst)
+
+    def calibration_up(self):
+        if not self.up_calb_flag:
+            lst = []
+            for i in range(100):
+                val = rd.read_test_ser(1)
+                lst.append(val)
+            self.pos_y0 = max(lst)
+            self.up_calb_flag = True
+            print(lst)
+
+    def calibration_down(self):
+        if not self.down_calb_flag:
+            lst = []
+            for i in range(100):
+                val = rd.read_test_ser(1)
+                lst.append(val)
+            self.pos_y1 = min(lst)
+            self.down_calb_flag = True
+            print(lst)
 
     def calibration_reset(self):
         self.left_calb_flag = False
         self.right_calb_flag = False
+        self.up_calb_flag = False
+        self.down_calb_flag = False
 
     def value_upd(self):
         if self.left_calb_flag and self.right_calb_flag:
-            new_data = rd.read_test_ser()  # シリアル読み取り
+            new_data = rd.read_test_ser(0)  # シリアル読み取り
             # センサ値から画面上のカーソル位置を計算
-            self.new_x = int((new_data - pos_x0) /
-                             (pos_x1-pos_x0) * window_size)
+            self.new_x = int((self.pos_x0 - new_data) /
+                             (self.pos_x0 - self.pos_x1) * window_size_x)
             if (self.new_x > 0 and self.new_x < 1280)and(abs(self.new_x - self.old_x) > 10):
                 self.update()
             self.old_x = self.new_x
             #print(new_data)
+
+        if self.up_calb_flag and self.down_calb_flag:
+            new_data = rd.read_test_ser(1)  # シリアル読み取り
+            # センサ値から画面上のカーソル位置を計算
+            self.new_y = int((self.pos_y0 - new_data) /
+                                (self.pos_y0 - self.pos_y1) * window_size_y)
+            if (self.new_y > 0 and self.new_y < 720)and(abs(self.new_y - self.old_y) > 10):
+                self.update()
+            self.old_y = self.new_y
+        #print(new_data)
+    '''        
+    def value_upd_y(self):
+        if self.up_calb_flag and self.down_calb_flag:
+            new_data = rd.read_test_ser(1)  # シリアル読み取り
+            # センサ値から画面上のカーソル位置を計算
+            self.new_y = int((self.pos_y0 - new_data) /
+                             (self.pos_y0 - self.pos_y1) * window_size_y)
+            if (self.new_y > 0 and self.new_y < 720)and(abs(self.new_y - self.old_y) > 10):
+                self.update()
+            self.old_y = self.new_y
+            #print(new_data)
+    '''
 
     def main(self):
         self.show()
@@ -125,8 +197,8 @@ class draw_gui(QWidget):
 
 
 rd = sensor_read()
-pos_x0 = 600  # 画面左端を示すセンサ値(仮)  x=0
-pos_x1 = 400  # 画面右端を示すセンサ値(仮)  x=1920
+#pos_x0 = 0  # 画面左端を示すセンサ値(仮)  x=0
+#pos_x1 = 0  # 画面右端を示すセンサ値(仮)  x=1920
 app = QApplication(sys.argv)
 window = draw_gui()
 
